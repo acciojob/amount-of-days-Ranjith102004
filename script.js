5ebc6302d1982b5a8de0a74d432b6a0a7b427abe
Ranjith <143510807+Ranjith102004@users.noreply.github.com>
2025-10-31 19:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function daysOfAYear(year) {
-	if (typeof year !== 'number' || year < 1 || year > 9999) {
-    throw new Error("Year must be an integer between 1 and 9999");
+  if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+    return 366;
+  } else {
+    return 365;
   }
-	if (year % 400 === 0 ) || ( year % 100 ===0 && year % 4 === 0){
-		return 366;
-	}
-	else{
-		return 365;
-	}
-	
 }
+
+
+const year = parseInt(prompt("Enter a year:"));
+
+
+alert(daysOfAYear(year));
