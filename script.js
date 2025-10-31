3ed895615d616bb516a94191e6812cb7feea67c4
Ranjith <143510807+Ranjith102004@users.noreply.github.com>
2025-10-31 19:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+	if (typeof year !== 'number' || year < 1 || year > 9999) {
+    throw new Error("Year must be an integer between 1 and 9999");
+  }
+	if (year % 400 === 0 ) || ( year % 100 ===0 && year % 4 === 0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+	
+}
